3f60beb414e2f6ba186a1fe88ce42dbb441e8ef8
Mohamed Elgendi <mohelgendi@hotmail.com>
2021-11-26 09:07:50 +0300

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,81 @@
+////////////Pseudo Code////////////
+//  #loop
+//  |- Pop the first element 'K' on the input list
+//  |- Loop from:'A' source to tagret:'B' of elemnt 'K'
+//     |- Every floor check if there's any elemnts in the list between 'A' and 'B' and add them to peopleInside[]
+//     |- Every floor check if there's any person with target matching current floor and remove it from peopleInside[]
+//////////////////////////////////
+
+let input = [
+    {
+      source: 2,
+      target: 6,
+      name: 'Mike'
+    },
+    {
+      source: 9,
+      target: 1,
+      name: 'Richard'
+    },
+    {
+      source: 6,
+      target: 3,
+      name: 'Jon'
+    },
+    {
+      source: 0,
+      target: 5,
+      name: 'Martin'
+    },
+  ]
+  function excludePersonFromList(person, list) {
+    list.forEach((el, i) => {
+      if (el.name === person.name) {
+        list.splice(i, 1)
+        return
+      }
+    })
+  }
+  
+  function getJoiners(floor, direction) {
+    input.forEach(person => {
+      let pDir = person.source > person.target ? -1: 1
+      if (person.source === floor && pDir  === direction) {
+        console.log(`${person.name} Stepped In..`)
+        peopleInElev.push(person)
+        excludePersonFromList(person, input)
+      }
+    })
+  }
+  
+  function getDroppers(floor, priortizedPerson) {
+    let priortizeFlag = false
+    peopleInElev.forEach(person => {
+      if (person.target === floor) {
+        console.log(`${person.name} Dropped Off..`)
+        excludePersonFromList(person, peopleInElev)
+        if(priortizedPerson.name === person.name) {
+          priortizeFlag = true
+        }
+      }
+    })
+    return priortizeFlag
+  }
+  
+  let peopleInElev = []
+  let from = 0
+  let x = 0
+  while (input.length > 0) {
+    if (x == 4) break// dummy break to stop infinit loop
+    let person = input[0]
+    let direction = person.source > person.target ? -1 : 1
+    let to = person.target
+    for (let floor = from; floor != to + direction; floor += direction) {
+      console.log(`We Are in Floor #${floor}`)
+      getJoiners(floor, direction)
+      if(getDroppers(floor, person)) {
+        from = person.target
+      }
+    }
+    x++
+  }
